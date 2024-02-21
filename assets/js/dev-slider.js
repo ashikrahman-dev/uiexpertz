06d531372031cf57b21d69f3846fa156d37a372a
ashikrahman-dev <ashikur1993@gmail.com>
2024-02-21 15:32:57 +0600

    Add 404 Not found page animation

--- assets/js/dev-slider.js
+++ assets/js/dev-slider.js
@@ -492,3 +492,32 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 // Portfolio image animation slide -3 -/End
+
+
+
+
+// 404 Error page js code -Start
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
+// 404 Error page js code -End
